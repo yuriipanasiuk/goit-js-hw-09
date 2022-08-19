f7bb9bbe45cf33cca4b2dada4f80d62de4a63ecc
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-08-19 13:54:44 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', startCahgeColor);
+refs.stopBtn.addEventListener('click', stopCahgeColor);
+
+let timerId = null;
+
+function startCahgeColor() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function stopCahgeColor() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
